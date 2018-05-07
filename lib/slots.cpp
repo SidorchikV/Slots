7fb1d0623abb9d9cdf677825225b8a197088b761
Виктория Сидорчик <Sidorchikv@asup.local>
2018-05-07 12:30:16 +0300

    working class

--- lib/slots.cpp
+++ lib/slots.cpp
@@ -3,6 +3,7 @@
 
 SlotGameParams::SlotGameParams(int _numOfReels, int _numOfFruit)
 {
+	srand(time(NULL));
 	numOfReels = _numOfReels;
 	numOfFruit = _numOfFruit;
 	reelsLength = vector<int>(_numOfReels);
@@ -35,6 +36,11 @@ bool SlotGameParams::readReelsValue(string * pathReelsValue)
 			}
 		}
 	}
+	numOfCombinations = 1;
+	for (int i = 0; i < numOfReels; i++)
+	{
+		numOfCombinations *= reelsLength[i];
+	}
 	return true;
 }
 
@@ -59,6 +65,89 @@ bool SlotGameParams::readWinTable(string * pathWinTable)
 	return true;
 }
 
+bool SlotGameParams::countProbabilityTable()
+{
+	for (int i = 0; i < numOfReels; i++)
+	{
+		for (int j = 0; j < reelsLength[i]; j++)
+		{
+			tableOfProb[i][(reelsValue[i][j] - 1)]++;
+		}
+		for (int j = 0; j < numOfFruit; j++)
+		{
+			tableOfProb[i][j] /= reelsLength[i];
+		}
+	}
+	return true;
+}
+
+double SlotGameParams::theoreticalWin()
+{
+	this->countProbabilityTable();
+	double theoreticalScore = 0;
+	vector<double> wins(numOfReels);
+	for (int f = 0; f < numOfFruit; f++)
+	{
+		wins = winTable[f];
+		for (int i = 0; i < numOfReels; i++)
+		{
+			for (int k = 0; k <= i; k++)
+			{
+				wins[i] *= tableOfProb[k][f];
+			}
+			if (i + 1 < numOfReels)
+			{
+				wins[i] *= (1 - tableOfProb[i + 1][f]);
+			}
+			theoreticalScore += wins[i];
+		}
+	}
+	return theoreticalScore;
+}
+
+double SlotGameParams::randomStartsWin(int numOfStarts)
+{
+	if (numOfStarts < 1)
+	{
+		numOfStarts = numOfCombinations;
+	}
+	double randomStartsScore = 0;
+	vector<int> tempComb(numOfReels);
+	for (int i = 0; i < numOfStarts; i++)
+	{
+		for (int j = 0; j < numOfReels; j++)
+		{
+			tempComb[j] = reelsValue[j][(rand()) % reelsLength[j]];
+		}
+		randomStartsScore += winTable[tempComb[0] - 1][checkLine(&tempComb) - 1];
+	}
+	return randomStartsScore / (numOfStarts);
+}
+
+double SlotGameParams::everyStartsWin()
+{
+	vector<int> tempComb(numOfReels);
+	double totalScore = 0;
+	make_permutation(0, &totalScore, &tempComb);
+	return totalScore/ numOfCombinations;
+}
+
+void SlotGameParams::make_permutation(int j, double *totalScore, vector<int>* tempComb)
+{
+	for (int i = 0; i < reelsLength[j]; i++)
+	{
+		(*tempComb)[j] = reelsValue[j][i];
+		if (j == (numOfReels - 1))
+		{
+			(*totalScore) += winTable[(*tempComb)[0] - 1][checkLine(tempComb) - 1];
+		}
+		else
+		{
+			make_permutation(j + 1, totalScore, tempComb);
+		}
+	}
+}
+
 int SlotGameParams::getNumOfReels()
 {
 	return numOfReels;
@@ -67,4 +156,13 @@ int SlotGameParams::getNumOfReels()
 int SlotGameParams::getNumOfFruit()
 {
 	return numOfFruit;
+}
+int checkLine(vector<int>* line)
+{
+	for (int i = 1; i < (*line).capacity(); i++)
+	{
+		if ((*line)[i - 1] != (*line)[i])
+			return i;
+	}
+	return (*line).capacity();
 }
--- lib/slots.h
+++ lib/slots.h
@@ -10,18 +10,24 @@ using namespace std;
 
 class SlotGameParams
 {
+	bool countProbabilityTable();
+	void make_permutation(int j, double *totalScore, vector<int>* tempComb);
 public:
 	int numOfReels;
 	int numOfFruit;
-
+	int numOfCombinations;
 	vector<int> reelsLength;
 	vector<vector<int>> reelsValue;
 	vector<vector<double>> winTable;
 	vector<vector<double>> tableOfProb;
-
 	SlotGameParams(int _numOfReels, int _numOfFruit);
 	bool readReelsValue(string *pathReelsValue);
 	bool readWinTable(string *pathWinTable);
+	double theoreticalWin();
+	double randomStartsWin(int numOfStarts);
+	double everyStartsWin();
 	int getNumOfReels();
 	int getNumOfFruit();
 };
+
+int checkLine(vector<int>* line);
