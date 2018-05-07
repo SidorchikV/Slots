76e4a3c4b81c1c79a67a479eee5643fae86a801e
Виктория Сидорчик <Sidorchikv@asup.local>
2018-05-07 15:41:44 +0300

    change test to test_slots

---
+++ test/test_slots.cpp
@@ -0,0 +1,22 @@
+#include "slots.h"
+
+int main()
+{
+	string pathReels = "../reels.txt";
+	string pathWinTable = "../winTable.txt";
+	SlotGameParams sampleSlot = SlotGameParams(3, 4);
+	try
+	{
+		sampleSlot.readWinTable(&pathWinTable);
+		sampleSlot.readReelsValue(&pathReels);
+		cout << "\nProbabilistic average win : " << sampleSlot.randomStartsWin(0) << endl;
+		cout << "\nReal average win : " << sampleSlot.everyStartsWin() << endl;
+		cout << "\nTheoretical average win : " << sampleSlot.theoreticalWin() << endl << endl;
+	}
+	catch (...)
+	{
+		cout << "error!" << endl;
+	}
+	system("pause");
+	return 0;
+}
