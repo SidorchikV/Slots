0b2aa6498d9fbda4b7d0c81b9a43dbfd9d577e83
Виктория Сидорчик <Sidorchikv@asup.local>
2018-05-11 19:22:47 +0300

    add tests

--- test/test_slots.cpp
+++ test/test_slots.cpp
@@ -1,23 +1,55 @@
 #include "slots.h"
 #include <iostream>
+#include <string>
+#include <fstream>
 
+typedef struct testParams
+{
+  size_t numOfReels;
+  size_t numOfFruit;
+  size_t numOfStarts;
+  size_t seed;
+};
+void readTestParam(std::string const & pathTestParam, testParams & tParams)
+{
+  std::ifstream fin(pathTestParam);
+  if (!fin.is_open())
+  {
+    throw std::invalid_argument("Can not open file with slot params. Check the path you entered: " + pathTestParam + "\n");
+  }
+  fin >> tParams.numOfReels;
+  fin >> tParams.numOfFruit;
+  fin >> tParams.numOfStarts;
+  fin >> tParams.seed;
+}
 int main()
 {
-  std::string pathReels = "../reels.txt";
-  std::string pathWinTable = "../winTable.txt";
-  SlotGameParams sampleSlot = SlotGameParams(3, 4);
-  try
+  std::string pathReels;
+  std::string pathWinTable;
+  std::string path = "../test/test_cases/";
+  testParams tP;
+  for (int i = 1; i <= 6; i++)
   {
-    sampleSlot.readWinTable(pathWinTable);
-    sampleSlot.readReelsValue(pathReels);
-    std::cout << "\nProbabilistic average win (default parameters): " << sampleSlot.approxRTP(0) << std::endl;
-    std::cout << "\nProbabilistic average win : " << sampleSlot.approxRTP(5000, 789) << std::endl;
-    std::cout << "\nReal average win : " << sampleSlot.calcRTP() << std::endl;
-    std::cout << "\nTheoretical average win : " << sampleSlot.estimateRTP() << std::endl << std::endl;
-  }
-  catch (std::exception err)
-  {
-    std::cout << err.what() << std::endl;
+    try
+    {
+      std::cout << "\nTest #" << i << std::endl;
+      pathReels = path + std::to_string(i) + "/reels.txt";
+      pathWinTable = path + std::to_string(i) + "/winTable.txt";
+      readTestParam(path + std::to_string(i) + "/testParams.txt", tP);
+      SlotGameParams sampleSlot(tP.numOfReels, tP.numOfFruit);
+      sampleSlot.readWinTable(pathWinTable);
+      sampleSlot.readReelsValue(pathReels);
+      std::cout << "\nCalculated RTP : " << sampleSlot.calcRTP();
+      std::cout << "\nEstimate RTP: " << sampleSlot.estimateRTP();
+      if (tP.seed <= 0)
+        std::cout << "\nApproximate RTP (default parameters): " << sampleSlot.approxRTP(tP.numOfStarts) << std::endl;
+      else
+        std::cout << "\nApproximate RTP: " << sampleSlot.approxRTP(tP.numOfStarts, tP.seed) << std::endl;
+    }
+    catch (std::exception err)
+    {
+      std::cout << err.what();
+    }
   }
   system("pause");
   return 0;
