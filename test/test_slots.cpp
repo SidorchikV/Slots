73ceeb0fe10a84f17d8c64532a39f9a326a47e74
Виктория Сидорчик <Sidorchikv@asup.local>
2018-05-16 15:55:06 +0300

    deleted testParams

--- test/test_slots.cpp
+++ test/test_slots.cpp
@@ -5,26 +5,6 @@
 #include <chrono>
 #include <random>
 
-typedef struct testParams
-{
-  size_t numOfReels;
-  size_t numOfFruit;
-  size_t numOfStarts;
-  size_t seed;
-};
-void readTestParam(std::string const & pathTestParam, testParams & tParams)
-{
-  std::ifstream fin(pathTestParam);
-  if (!fin.is_open())
-  {
-    throw std::invalid_argument("Can not open file with slot params. Check the path you entered: " + pathTestParam + "\n");
-  }
-  fin >> tParams.numOfReels;
-  fin >> tParams.numOfFruit;
-  fin >> tParams.numOfStarts;
-  fin >> tParams.seed;
-}
-
 testStruct sampleTP1 = { {{ 1, 2, 4, 4, 3},{2, 3, 1, 2, 4, 3},{3, 2, 1, 4, 4, 2} }, 
 { {1, 2, 7},{0, 1, 8},{0, 3, 6},{0, 2, 8} },
 0.983333 };
@@ -42,7 +22,7 @@ int main()
       {
         //std::cout << i * 5 + j + 1 << " :\n";
         //std::cout << randomParamsTest(std::chrono::system_clock::now().time_since_epoch().count(), pow(10, i + 2), "../test/randtests.txt") << std::endl << std::endl;
-        randomParamsTest(std::chrono::system_clock::now().time_since_epoch().count(), pow(10, i + 2), "../test/randtests.txt");
+        randomParamsTest(std::chrono::system_clock::now().time_since_epoch().count(), pow(10, i + 2)/*, "../test/randtests.txt"*/);
       }
     }
   }
