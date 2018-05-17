f8d725f50b9e98dcd5de272cdaaca80a25de1528
Vlad Semenov <semenov.vlad.by@gmail.com>
2018-05-18 00:06:48 +0300

    code review

--- lib/slots.h
+++ lib/slots.h
@@ -1,24 +1,33 @@
 #pragma once
 #include <vector>
 
-typedef std::vector<std::vector<size_t>> reels_t;
-typedef std::vector<std::vector<size_t>> wins_t;
+typedef std::vector<size_t> reel_t;
+typedef std::vector<reel_t> reels_t;
+typedef std::vector<size_t> iline_t;
+typedef std::vector<size_t> line_t;
+typedef std::vector<size_t> win_line_t;
+typedef std::vector<win_line_t> wins_t;
+typedef std::vector<std::vector<size_t>> prob_table_t;
 typedef std::pair<size_t, size_t> rational_t;
+typedef std::vector<size_t> denominators_t;
 
 reels_t readReels(std::istream &fin);
-wins_t readWinTable(std::istream &fin);
+wins_t readWinTable(std::istream &fin, size_t reels_size);
 
 class SlotGameParams
 {
   reels_t reels;
   wins_t winTable;
-  bool inc(std::vector<size_t> &line);
-  std::vector<std::vector<size_t>> countProbabilityTable();
-  std::vector<size_t> countDenominators();
+
+  bool inc(iline_t &line) const;
+  prob_table_t countProbabilityTable() const;
+  denominators_t countDenominators() const;
+
   SlotGameParams();
-  SlotGameParams(size_t _numOfReels, size_t _numOfFruit);
+  SlotGameParams(size_t numOfReels, size_t numOfFruit);
 public:
-  SlotGameParams(reels_t &reels, wins_t &winTable);
+  SlotGameParams(reels_t const &reels, wins_t const &winTable);
+  static SlotGameParams randomReels(unsigned seed = 31);
 
   SlotGameParams(SlotGameParams&& o);
   SlotGameParams &operator=(SlotGameParams &&o);
@@ -27,16 +36,12 @@ public:
 
   size_t numOfReels() const;
   size_t numOfFruit() const;
-  size_t reelsLength(size_t i) const;
+  size_t reelLength(size_t reel_no) const;
   size_t numOfCombinations() const;
 
-  rational_t calcRTPUsingFrequency();
-  rational_t estimateRTP(size_t numOfStarts, unsigned seed = 42);
-  rational_t calcRTPBruteForce();
-  static bool pointTest(reels_t reels, wins_t winTable, rational_t realRTP);
+  rational_t calcRTPUsingFrequency() const;
+  rational_t estimateRTP(size_t numOfStarts, unsigned seed = 42) const;
+  rational_t calcRTPBruteForce() const;
   //static SlotGameParams readReels(std::istream &fin1, std::istream &fin2);
-  static SlotGameParams randomReels(unsigned seed = 31);
 };
 
-size_t checkLine(std::vector<size_t> const &line);
-bool randomParamsTest(unsigned seed = 31, size_t numOfStarts = 0, std::string const &pathreels = "");
